51935f8d318441b02329342924bcc954ee92cb29
guixueyang3.@gmail.com <guixueyang3.gmail.com>
2020-09-08 10:42:45 +0900

    搭建项目架构

---
+++ msrshop-coupon/src/main/java/com/mst/msrshop/coupon/MsrshopCouponApplication.java
@@ -0,0 +1,13 @@
+package com.mst.msrshop.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrshopCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsrshopCouponApplication.class, args);
+    }
+
+}
---
+++ msrshop-coupon/src/test/java/com/mst/msrshop/coupon/MsrshopCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst.msrshop.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsrshopCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
